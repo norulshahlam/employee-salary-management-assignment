5fc3eed5a5684bd82f2283298a2b22424ef87f69
norulshahlam <norulshahlam@gmail.com>
2023-03-26 05:30:53 +0800

    refactor

--- src/main/java/com/shah/employeesalarymanagementassignment/Application.java
+++ src/main/java/com/shah/employeesalarymanagementassignment/Application.java
@@ -7,10 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author NORUL
  */
 @SpringBootApplication
-public class EmployeeSalaryManagementAssignmentApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeSalaryManagementAssignmentApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/shah/employeesalarymanagementassignment/ApplicationTests.java
+++ src/test/java/com/shah/employeesalarymanagementassignment/ApplicationTests.java
@@ -3,7 +3,7 @@ package com.shah.employeesalarymanagementassignment;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EmployeeSalaryManagementAssignmentApplicationTests {
+class ApplicationTests {
 
 
 
